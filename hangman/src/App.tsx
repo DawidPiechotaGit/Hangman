054987ce1bb1de74374a48fc558d97ffeea8f017
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-05-02 13:26:22 +0200

    feat: hangman drawing + components created

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import words from "./wordList.json";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
+import { Keyboard } from "./components/Keyboard";
+
 function App() {
-  return <h1>Hi</h1>;
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  console.log(wordToGuess);
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
